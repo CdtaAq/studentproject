b8ede7dd68fabb159551a7221a7f254948e79eb4
chris Taylor <ctay456a@gmail.com>
2025-08-09 01:44:31 -0400

    Create Student.java

---
+++ src/main/java/com/example/StudentProject/model/Student.java
@@ -0,0 +1,35 @@
+package com.example.StudentProject.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String trainer;
+
+    public Student() {}
+
+    public Student(String name, int age, String gender, String trainer) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.trainer = trainer;
+    }
+
+    // getters and setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+    public String getTrainer() { return trainer; }
+    public void setTrainer(String trainer) { this.trainer = trainer; }
+}
